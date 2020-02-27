ed527e7dfd5c6b02d14599c1f9b01f39ad348208
Muragul <muragulm@gmail.com>
2020-02-27 23:12:52 +0600

    [Lab 5]: Fragments

---
+++ SaveNews/app/src/main/java/com/example/savenews/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.savenews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NewsListAdapter.FragmentButtonListener,
+SavesListAdapter.FragmentLikeListener{
+    private ViewPager pager;
+
+    private PagerAdapter pagerAdapter;
+    Fragment f1 = new NewsList();
+    Fragment f2 = new SavesList();
+    List<Fragment> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list.add(f1);
+        list.add(f2);
+        pager = findViewById(R.id.pager);
+        pagerAdapter = new SlidePagerAdapter(getSupportFragmentManager(), list);
+
+        pager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void myClick(News news, int option) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.pager);
+        if (option==1)
+            ((SavesList)fragment).saveNews(news);
+        else
+            ((SavesList)fragment).removeNews(news);
+    }
+
+    @Override
+    public void removeItemLike(News news) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.pager);
+        ((SavesList)fragment).removeNews(news);
+    }
+}
---
+++ SaveNews/app/src/main/java/com/example/savenews/News.java
@@ -0,0 +1,90 @@
+package com.example.savenews;
+
+import java.io.Serializable;
+
+public class News implements Serializable {
+    private String author;
+    private String date;
+    private int profilePhoto;
+    private int likesCnt;
+    private int postImage;
+    private String postText;
+    private boolean isLiked;
+    private boolean isSaved;
+
+    public boolean isLiked() {
+        return isLiked;
+    }
+
+    public void setLiked(boolean liked) {
+        isLiked = liked;
+    }
+
+    public boolean isSaved() {
+        return isSaved;
+    }
+
+    public void setSaved(boolean saved) {
+        isSaved = saved;
+    }
+
+    public News(String author, String date, int profilePhoto, int likesCnt, int postImage, String postText) {
+        this.author = author;
+        this.date = date;
+        this.profilePhoto = profilePhoto;
+        this.likesCnt = likesCnt;
+        this.postImage = postImage;
+        this.postText = postText;
+        this.isLiked = false;
+        this.isSaved = false;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(int profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public int getLikesCnt() {
+        return likesCnt;
+    }
+
+    public void setLikesCnt(int likesCnt) {
+        this.likesCnt = likesCnt;
+    }
+
+    public int getPostImage() {
+        return postImage;
+    }
+
+    public void setPostImage(int postImage) {
+        this.postImage = postImage;
+    }
+
+    public String getPostText() {
+        return postText;
+    }
+
+    public void setPostText(String postText) {
+        this.postText = postText;
+    }
+}
+
